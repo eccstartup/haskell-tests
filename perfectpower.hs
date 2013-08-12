d8590856ce12a5b7d4bead495e9a8a0a7399bc66
eccstartup <zhiwudazhanjiangshi@gmail.com>
2013-08-12 09:29:32 +0800

    modifying perfectpower.hs

--- perfectpower.hs
+++ perfectpower.hs
@@ -1,10 +1,10 @@
 import Data.Numbers.Primes
 import Data.List
 
-perfect n = 
-  let s = helper1 (primeFactors n)
-    in let a = head s > 1 
-      in let b = head s == last s
-        in a && b
+perfect n = helper2 n > 1
            
-helper1 l = sort (map length (group l))
+helper1 l = sort $ map length (group l)
+
+helper2 l = 
+  let s = helper1 $ primeFactors l 
+    in foldr gcd (head s) s
